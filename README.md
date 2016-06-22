42d6b3056e16526e39642d7f6e52d7c3738e0fe2
Tu.Tran <tu.tran@orientsoftware.net>
2016-06-22 19:51:23 +0700

    remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# angular2_gettingstart
-introduce the basic about angular: Route, Component, Binding, Service and Http. You can be able to learn deeper later by yourself
--- app/layout.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from "angular2/core";
-@Component({
-    selector: "default-layout",
-    templateUrl: "app/layout.html"
-})
-export class DefaultLayout {
-}
